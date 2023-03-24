8043b9a25ece754c0de40c8824287da8e6656105
solidus66 <66solidus@gmail.com>
2023-03-24 19:10:02 +0300

    added 1st & 2nd tasks

---
+++ task_1(SaddlePoint)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число столбцов в матрице: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число колонок в матрице: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        Console.WriteLine("Введите элементы матрицы:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"Введите элемент [{i}, {j}]: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            int min = matrix[i, 0]; 
+            int colIndex = 0;
+
+            for (int j = 1; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] < min)
+                {
+                    min = matrix[i, j];
+                    colIndex = j;
+                }
+            }
+
+            bool isSaddlePoint = true;
+
+            for (int k = 0; k < matrix.GetLength(0); k++)
+            {
+                if (matrix[k, colIndex] > min)
+                {
+                    isSaddlePoint = false;
+                    break;
+                }
+            }
+
+            if (isSaddlePoint)
+            {
+                Console.WriteLine($"Седловая точка найдена на ({i}, {colIndex}) со значением {min}");
+            }
+        }
+        Console.ReadKey();
+    }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите прописную русскую букву: ");
+        char letter = Console.ReadLine().ToLower()[0];
+        Console.Write("Выберите, должна ли буква быть в начале (1) или в конце (2) слова: ");
+        int position = int.Parse(Console.ReadLine());
+
+        string inputFile = "C:\\Users\\solidus66\\OneDrive\\ВГУ\\3 курс 2 сем\\ИТ (Вахтин)\\tasks\\solidus_tasks\\task2\\input.txt";
+        string outputFile = "C:\\Users\\solidus66\\OneDrive\\ВГУ\\3 курс 2 сем\\ИТ (Вахтин)\\tasks\\solidus_tasks\\task2\\output.txt";
+
+        Encoding encoding = Encoding.UTF8;
+        StreamReader reader = new StreamReader(inputFile, encoding);
+        StreamWriter writer = new StreamWriter(outputFile, false, encoding);
+
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] words = line.Split(' ', ',', '.', ':', ';', '-', '!', '?', '\t');
+            foreach (string word in words)
+            {
+                if (word.Length == 0) continue;
+
+                char firstChar = char.ToLower(word[0]);
+                char lastChar = char.ToLower(word[word.Length - 1]);
+
+                if (position == 1 && firstChar == letter || position == 2 && lastChar == letter)
+                {
+                    writer.Write(word);
+                    writer.Write(' ');
+                }
+            }
+        }
+
+        reader.Close();
+        writer.Close();
+    }
+}
